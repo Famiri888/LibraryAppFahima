8c296491c926db0f39906ecaa222a45fdbee28a3
FAmiri <amiri.fahima1@gmail.com>
2025-02-05 11:25:46 -0500

    first commit

---
+++ src/main/java/com/LibraryApp/FahimaLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package com.LibraryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FahimaLibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FahimaLibraryAppApplication.class, args);
+	}
+
+}
